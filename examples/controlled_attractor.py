8b314380e5811dd8061e49229a72ccad65746b3d
James Bergstra <james.bergstra@gmail.com>
2013-08-09 17:57:23 -0400

    more work on controlled_attractor

--- examples/controlled_attractor.py
+++ examples/controlled_attractor.py
@@ -58,11 +58,11 @@ class SimClamp(object):
 
 def main():
 
-    N_NEURONS = 2000
-    N_POSITIONS = 20  # -- position is grid of this many points
-    N_BASIS = 8       # -- represent position in terms of this many Fourier components
+    N_NEURONS = 1000
+    N_POSITIONS = 8  # -- position is grid of this many points
+    N_BASIS = 4       # -- represent position in terms of this many Fourier components
     clamp_dt = 0.001   # -- holds matrix rows this many secs
-    clamp_steps = 2000
+    clamp_steps = 20000
 
     # 1: create signals
     basis_signal = Signal(N_BASIS * 2, dtype='float')
@@ -89,7 +89,8 @@ def main():
     # 4: train encoder/decoder circuit
 
     # 4.1: Sample a possible trajectory
-    data_directions = (rng.rand(clamp_steps) * 2 - 1) / 10
+    data_directions = (rng.randn(clamp_steps))
+    data_directions = scipy.signal.lfilter(np.ones(64)/256.0, [1], data_directions)
     data_positions_real = np.cumsum(data_directions)
     data_positions_table = np.zeros((len(data_positions_real), N_POSITIONS))
     for pos, table in zip(data_positions_real, data_positions_table):
@@ -100,16 +101,9 @@ def main():
         table[(posi + 1) % N_POSITIONS] = posf
 
     # 4.3: Convert data positions to Fourier domain
-    blobs = scipy.signal.lfilter([.15, .7, .15], [1], data_positions_table)
-    basis_blobs = scipy.fftpack.fft(blobs)
+    #blobs = scipy.signal.lfilter([.15, .7, .15], [1], data_positions_table)
+    basis_blobs = scipy.fftpack.fft(data_positions_table)
     basis_blobs[:, N_BASIS:] = 0
-    if 1:
-        plt.subplot(1, 2, 1)
-        plt.plot(data_positions_real)
-        plt.subplot(1, 2, 2)
-        d2 = scipy.fftpack.ifft(basis_blobs)
-        plt.imshow(np.real(d2))
-        plt.show()
     data_basis = np.empty((len(basis_blobs), 2 * N_BASIS))
     data_basis[:, ::2] = np.real(basis_blobs[:, :N_BASIS])
     data_basis[:, 1::2] = np.imag(basis_blobs[:, :N_BASIS])
@@ -117,8 +111,24 @@ def main():
     data_basis_next[:-1] = data_basis[1:]
     data_basis_next[-1] = data_basis[0]
 
+    d2 = scipy.fftpack.ifft(basis_blobs)
+    if 0:
+        plt.subplot(1, 4, 1)
+        plt.plot(data_positions_real)
+        plt.title('pos(t)')
+        plt.subplot(1, 4, 2)
+        plt.imshow(data_basis, aspect='auto')
+        plt.title('fft(pos table)')
+        plt.subplot(1, 4, 3)
+        plt.imshow(np.real(d2), aspect='auto')
+        plt.title('ifft(fft(pos table))')
+        plt.show()
+
+    direction_coef = 1.0 / np.max(abs(data_directions))
+
     clamp_basis = Clamp(np.asarray(data_basis), dt=clamp_dt)
-    clamp_direc = Clamp(np.asarray(data_directions)[:, None], dt=clamp_dt)
+    clamp_direc = Clamp(np.asarray(data_directions)[:, None] * direction_coef,
+                        dt=clamp_dt)
     clamp_basis_out = Clamp(np.asarray(data_basis_next), dt=clamp_dt)
     clamp_positions = Clamp(np.asarray(data_positions_table), dt=clamp_dt)
 
@@ -147,24 +157,26 @@ def main():
     simple_filter_decoded_signal(training_model, basis_signal, pstc=.01)
     simple_filter_decoded_signal(training_model, position_signal, pstc=.01)
 
-    p1 = probe_nl(training_model, clamp_direc)
-    p2 = probe_nl(training_model, clamp_basis)
-    p3 = probe_nl(training_model, basis_neurons)
-    p4 = training_model.probe(basis_signal, dt=0)
-    p5 = probe_nl(training_model, clamp_basis_out)
-    p6 = probe_nl(training_model, clamp_positions)
+    p_clamp_direc = probe_nl(training_model, clamp_direc)
+    p_clamp_basis = probe_nl(training_model, clamp_basis)
+    p_basis_neurons = probe_nl(training_model, basis_neurons)
+    p_basis_signal = training_model.probe(basis_signal, dt=0)
+    p_clamp_basis_out = probe_nl(training_model, clamp_basis_out)
+    p_clamp_positions = probe_nl(training_model, clamp_positions)
 
-    req_steps = int(N_POSITIONS * 4 * clamp_dt / training_model.dt)
-    print 'Running for steps:', req_steps
     sim = Simulator(training_model)
-    sim.run_steps(req_steps)
+    sim.run_steps(clamp_steps)
 
-    A = np.asarray(sim.probe_data(p3))
-    d5 = sim.probe_data(p5)
-    d6 = sim.probe_data(p6)
+    A = np.asarray(sim.probe_data(p_basis_neurons))
+    d_clamp_basis_out = sim.probe_data(p_clamp_basis_out)
+    d_clamp_positions = sim.probe_data(p_clamp_positions)
 
-    basis_readout_weights, res5, rank5, s5 = np.linalg.lstsq(A, d5, rcond=0.01)
-    posit_readout_weights, res6, rank6, s6 = np.linalg.lstsq(A, d6, rcond=0.01)
+    basis_readout_weights, res5, rank5, s5 = np.linalg.lstsq(A,
+                                                             d_clamp_basis_out,
+                                                             rcond=0.01)
+    posit_readout_weights, res6, rank6, s6 = np.linalg.lstsq(A,
+                                                             d_clamp_positions,
+                                                             rcond=0.01)
 
     dec_basis = Decoder(basis_neurons, basis_signal,
                         weights=basis_readout_weights.T)
@@ -173,17 +185,22 @@ def main():
 
     #print sim.probe_outputs[p]
 
-    if 1:
+    if 0:
         plt.subplot(1, 4, 1)
-        plt.imshow(sim.probe_data(p1), extent=[0, 100, 0, 1], aspect='auto')
+        plt.imshow(sim.probe_data(p_clamp_basis), extent=[0, N_BASIS * 2, 0, 1],
+                   aspect='auto')
         plt.subplot(1, 4, 2)
-        plt.imshow(sim.probe_data(p2), extent=[0, 100, 0, 1], aspect='auto')
+        plt.imshow(sim.probe_data(p_basis_neurons), extent=[0, N_NEURONS, 0, 1],
+                   aspect='auto')
         plt.subplot(1, 4, 3)
-        plt.imshow(sim.probe_data(p3), extent=[0, 100, 0, 1], aspect='auto')
+        plt.imshow(sim.probe_data(p_clamp_basis_out), extent=[0, N_BASIS * 2, 0, 1],
+                   aspect='auto')
         plt.subplot(1, 4, 4)
-        plt.imshow(sim.probe_data(p4), extent=[0, 100, 0, 1], aspect='auto')
+        plt.imshow(np.dot(A, basis_readout_weights), extent=[0, N_BASIS * 2, 0, 1],
+                   aspect='auto')
         #print sim.probe_data(p4)
         plt.show()
+        #sys.exit()
 
     testing_model = SimModel()
     testing_model.signals.update(
@@ -191,15 +208,15 @@ def main():
          basis_neurons.input_signal, basis_neurons.bias_signal,
          basis_neurons.output_signal,
         ])
-    testing_model.nonlinearities.update([basis_neurons,
-                                        ])
+    testing_model.nonlinearities.update([basis_neurons, ])
     testing_model.encoders.update([enc_basis, enc_direc])
     testing_model.decoders.update([dec_basis, dec_posit])
-    simple_filter_decoded_signal(testing_model, basis_signal, pstc=.01)
-    simple_filter_decoded_signal(testing_model, position_signal, pstc=.01)
+    simple_filter_decoded_signal(testing_model, basis_signal, pstc=.005)
+    simple_filter_decoded_signal(testing_model, position_signal, pstc=.005)
 
     # -- add direction clamp (using feedback for basis)
     #clamp_direc.dt /= 2  # -- slow it down rel to training, test generalization
+    clamp_direc.data[:] = 0.2
     testing_model.signals.add(clamp_direc.output_signal)
     testing_model.nonlinearities.add(clamp_direc)
     testing_model.decoder(clamp_direc, direction_signal, 1.0)
@@ -207,24 +224,37 @@ def main():
 
     tp_basis = testing_model.probe(basis_signal, dt=0)
     tp_posit = testing_model.probe(position_signal, dt=0)
-    #import pdb; pdb.set_trace()
     tp_neurons = probe_nl(testing_model, basis_neurons)
+    tp_dir = testing_model.probe(direction_signal, dt=0)
 
     test_sim = Simulator(testing_model)
-    test_sim.run_steps(5000)
+    test_sim.run_steps(1000)
     
     #print test_sim.probe_outputs.keys()
     #print test_sim.probe_data(tp_neurons)
 
     if 1:
-        plt.subplot(1, 2, 1)
-        plt.imshow(test_sim.probe_data(tp_basis), extent=[0, N_BASIS, 0, 1],
-                   aspect='auto')
-        plt.subplot(1, 2, 2)
-        plt.imshow(test_sim.probe_data(tp_posit), extent=[0, N_POSITIONS, 0, 1],
-                   aspect='auto')
-        #plt.subplot(1, 3, 3)
-        #plt.imshow(test_sim.probe_data(tp_neurons))
+        plt.subplot(2, 4, 1)
+        plt.plot(data_positions_real)
+        plt.title('pos(t)')
+        plt.subplot(2, 4, 2)
+        plt.imshow(data_basis, aspect='auto')
+        plt.title('fft(pos table)')
+        plt.subplot(2, 4, 3)
+        plt.imshow(np.real(d2), aspect='auto')
+        plt.title('ifft(fft(pos table))')
+
+        plt.subplot(2, 4, 5)
+        plt.plot(test_sim.probe_data(tp_dir).ravel())
+
+        plt.subplot(2, 4, 6)
+        plt.imshow(test_sim.probe_data(tp_basis), aspect='auto')
+        plt.title('basis readout')
+
+        plt.subplot(2, 4, 7)
+        plt.imshow(test_sim.probe_data(tp_posit), aspect='auto')
+        plt.title('position readout')
+
         plt.show()
 
 if __name__ == '__main__':
